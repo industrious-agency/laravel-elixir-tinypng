7d3ed840831dcefd3c133b7086a2f4099929281e
Christian Thomas <christian@industriousmouse.co.uk>
2017-04-20 19:13:46 +0100

    Update formatting

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var gulp     = require('gulp');
 var _        = require('underscore');
 var Elixir   = require('laravel-elixir');
-var tinypng	 = require('gulp-tinypng-compress');
+var tinypng  = require('gulp-tinypng-compress');
 
 Elixir.extend('tinypng', function(options) {
+  options = _.extend({
+    src: Elixir.config.assetsPath + '/img',
+    output: Elixir.config.publicPath + '/img',
+    tinypngKey: ''
+  }, options);
 
-		options = _.extend({
-			src: Elixir.config.assetsPath + '/img',
-			output: Elixir.config.publicPath + '/img',
-			tinypngKey: ''
-		}, options);
+  new Elixir.Task('tinypng', function () {
+    this.recordStep('Starting Tinypng');
 
-    new Elixir.Task('tinypng', function () {
-			return gulp.src(options.src + '/**/*.{png,jpg,jpeg}')
-				.pipe(tinypng({
-						key: options.tinypngKey,
-						checkSigs: true,
-						sigFile: options.src + '/.tinypng-sigs',
-						log: true
-					}))
-				.pipe(gulp.dest(options.output))
-				.pipe(new Elixir.Notification('Tinypng Complete!'))
-    })
-    .watch(options.src + '/**/*.{png,jpg,jpeg}')
+    return gulp.src(options.src + '/**/*.{png,jpg,jpeg}')
+      .pipe(tinypng({
+        key: options.tinypngKey,
+        checkSigs: true,
+        sigFile: options.src + '/.tinypng-sigs',
+        log: true
+      }))
+      .pipe(gulp.dest(options.output))
+      .pipe(new Elixir.Notification('Tinypng Complete!'));
+  })
+  .watch(options.src + '/**/*.{png,jpg,jpeg}')
 });
-
